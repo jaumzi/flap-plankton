79d091b52ae5e79292a87d514c909e49ac529478
jaumzi <joaovribeirojv@gmail.com>
2021-06-24 18:57:48 -0300

    final versão 1, iniciar versão 2

--- scripts/game.js
+++ scripts/game.js
@@ -12,102 +12,140 @@ const convertRemToPixels = (rem) => {
   return rem > 0 ? (rem * convertDistanceToNumber(getComputedStyle(document.documentElement).fontSize)) : 0;
 }
 
-// variables
-const jumpDistance = convertRemToPixels(10);
-const jumpDecay = convertRemToPixels(4);
-const itemsDecay = convertRemToPixels(4);
-let itemsInFrame = 0;
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
 
-const handlePlanktonJump = () => {
-  const element = document.getElementById('plankton');
-  let marginBottom = convertDistanceToNumber(getElementStyle(element, 'margin-bottom'));
-
-  let distance = 0;
-  if (!marginBottom) {
-    distance = jumpDistance;
-  } else {
-    distance = marginBottom += jumpDistance;
-  }
-
-  const maxDistance = document.body.clientHeight;
-  if ((distance + (element.offsetHeight / 2)) >= maxDistance) {
-    distance = (maxDistance - (element.offsetHeight / 2));
-  }
-
-  element.style.marginBottom = convertPixelsToEm(distance) + 'em';
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
-document.addEventListener('keydown', function (event) {
-  handlePlanktonJump();
-});
-
-const handleJumpDecay = () => {
-  const element = document.getElementById('plankton');
-
-  // execute jump decay
-  var distance = convertDistanceToNumber(getElementStyle(element, 'margin-bottom'));
-  distance -= jumpDecay;
-  if (distance <= 0) {
-    distance = 0;
-  }
-
-  element.style.marginBottom = convertPixelsToEm(distance) + 'em';
-}
-
-const handleAddItemsScenario = () => {
-  const contentHtml = document.getElementById('game-root').getElementsByClassName('content')[0];
-
-  const templateItemTop = `
-    <div class="item pineapple item-top">
-      <img class="pineapple" src="./assets/pineapple.png" alt="Pineapple" />
-    </div>
-  `;
-  contentHtml.innerHTML += templateItemTop;
-
-  const templateItemMiddle = `
-    <div class="item item-middle">
-      <img class="siriqueijo" src="./assets/siriqueijo.png" alt="Siriqueijo" />
-    </div>
-  `;
-  contentHtml.innerHTML += templateItemMiddle;
-
-  const templateItemBottom = `
-    <div class="item pineapple item-bottom">
-      <img class="pineapple" src="./assets/pineapple.png" alt="Pineapple" />
-    </div>
-  `;
-  contentHtml.innerHTML += templateItemBottom;
-}
-
-const handleScenarioMoviment = () => {
-  const items = document.getElementsByClassName('item');
-  const maxDistance = document.body.clientWidth;
+const deleteHtml = (classHtml) => {
+  const items = document.getElementsByClassName(classHtml);
 
   for (var i = 0; i < items.length; i++) {
-    const element = items[i];
-    let distance = itemsDecay + convertDistanceToNumber(element.style.right);
-
-    if (distance >= maxDistance) { // bateu no final da tela
-      distance = maxDistance; // deixa passar até sair da visão
-    }
-
-    element.style.right = distance + 'px';
+    items[i].remove();
   }
 }
 
-const gameRunTime = () => {
-  handleJumpDecay();
+class Game {
+  // variables
+  jumpDistance = convertRemToPixels(10);
+  jumpDecay = convertRemToPixels(4);
+  itemsDecay = convertRemToPixels(4);
+  itemsInFrame = 0;
 
-  if (itemsInFrame < 1) {
-    handleAddItemsScenario();
-    itemsInFrame++;
-  }
+  handleJumpDecay = () => {
+    const element = document.getElementById('plankton');
 
-  handleScenarioMoviment();
+    // execute jump decay
+    var distance = convertDistanceToNumber(getElementStyle(element, 'margin-bottom'));
+    distance -= this.jumpDecay;
+    if (distance <= 0) {
+      distance = 0;
+    }
 
-};
+    element.style.marginBottom = convertPixelsToEm(distance) + 'em';
+  };
+
+  handlePlanktonJump = () => {
+    const element = document.getElementById('plankton');
+    let marginBottom = convertDistanceToNumber(getElementStyle(element, 'margin-bottom'));
+
+    let distance = 0;
+    if (!marginBottom) {
+      distance = this.jumpDistance;
+    } else {
+      distance = marginBottom += this.jumpDistance;
+    }
+
+    const maxDistance = document.body.clientHeight;
+    if ((distance + (element.offsetHeight / 2)) >= maxDistance) {
+      distance = (maxDistance - (element.offsetHeight / 2));
+    }
+
+    element.style.marginBottom = convertPixelsToEm(distance) + 'em';
+  };
+
+  handleAddItemsScenario = () => {
+    const contentHtml = document.getElementById('game-root').getElementsByClassName('content')[0];
+
+    const templateItemTop = `
+      <div class="item pineapple item-top">
+        <img class="pineapple" src="./assets/pineapple.png" alt="Pineapple" />
+      </div>
+    `;
+    const templateItemMiddle = `
+      <div class="item item-middle">
+        <img class="siriqueijo" src="./assets/siriqueijo.png" alt="Siriqueijo" />
+      </div>
+    `;
+    const templateItemBottom = `
+      <div class="item pineapple item-bottom">
+        <img class="pineapple" src="./assets/pineapple.png" alt="Pineapple" />
+      </div>
+    `;
+
+    let templates = [templateItemTop, templateItemMiddle, templateItemBottom];
+    const firstIndex = getRandomInt(0, templates.length);
+    contentHtml.innerHTML += templates[firstIndex];
+
+    templates = templates.filter(template => template !== templates[firstIndex]);
+    const secondIndex = getRandomInt(0, templates.length);
+    contentHtml.innerHTML += templates[secondIndex];
+
+    this.itemsInFrame++;
+  };
+
+  handleScenarioMoviment = () => {
+    const items = document.getElementsByClassName('item');
+    const maxDistance = document.body.clientWidth;
+
+    let itemsInScenarioFrame = true;
+
+    for (var i = 0; i < items.length; i++) {
+      const element = items[i];
+      let distance = this.itemsDecay + convertDistanceToNumber(element.style.right);
+
+      if (distance >= maxDistance) { // bateu no final da tela
+        distance = maxDistance; // deixa passar até sair da visão
+        itemsInScenarioFrame = false;
+      }
+
+      element.style.right = distance + 'px';
+    }
+
+    if (!itemsInScenarioFrame) {
+      this.deleteItemsScenario();
+    }
+  };
+
+  deleteItemsScenario = () => {
+    deleteHtml('item-top');
+    deleteHtml('item-middle');
+    deleteHtml('item-bottom');
+
+    this.itemsInFrame -= (this.itemsInFrame > 0) ? 1 : 0;
+  };
+
+  gameRunTime = () => {
+    this.handleJumpDecay();
+
+    if (this.itemsInFrame < 1) {
+      this.handleAddItemsScenario();
+    }
+
+    this.handleScenarioMoviment();
+  };
+}
+
+
+const game = new Game();
+
+document.addEventListener('keydown', function (event) {
+  game.handlePlanktonJump();
+});
 
 // game runtime
 setInterval(() => {
-  gameRunTime();
+  game.gameRunTime();
 }, 200);
